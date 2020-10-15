74e894bd4f2eaf5a7a378bed0dbd845088a1d776
Busola Olowu <bolowu@parthianpartnersng.com>
2020-10-15 07:30:46 +0100

    create categories

--- server/src/controller/category.js
+++ server/src/controller/category.js
@@ -1,6 +1,28 @@
-const Category = require("../models/category");
-const slugify = require("slugify");
+const Category = require('../models/category');
+const slugify = require('slugify');
 
+const createCategories = (categories, parentId = null) => {
+  try {
+    const categoryList = [];
+    let category;
+    if (parentId == null) {
+      category = categories.filter((cat) => cat.parentId == undefined);
+    } else {
+      category = categories.filter((cat) => cat.parentId == parentId);
+    }
+    for (let cat of category) {
+      categoryList.push({
+        _id: cat._id,
+        name: cat.name,
+        slug: cat.slug,
+        children: createCategories(categories, cat._id),
+      });
+    }
+    return categoryList;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 exports.addCategory = (req, res) => {
   try {
     const { name, parentId } = req.body;
@@ -13,7 +35,7 @@ exports.addCategory = (req, res) => {
     }
     const cat = new Category(categoryObj);
     cat.save((error, category) => {
-      if (error) return res.status(400).json({ message: "Duplicate category" });
+      if (error) return res.status(400).json({ message: 'Duplicate category' });
       if (category) {
         return res.status(201).json({ category });
       }
@@ -30,7 +52,8 @@ exports.getCategories = async (req, res) => {
     await Category.find((error, categories) => {
       if (error) return res.status(400).json({ error });
       if (categories) {
-        return res.status(200).json({ categories });
+        const categoryList = createCategories(categories);
+        return res.status(200).json({ categoryList });
       }
     });
   } catch (error) {
